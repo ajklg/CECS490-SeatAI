7127eec507d97155308502861c42534c95721cbe
ajklg <48529450+ajklg@users.noreply.github.com>
2022-04-24 20:25:26 -0700

    Final(?) Website update

--- ChairWebsite/ChairSite/ChairApp/views.py
+++ ChairWebsite/ChairSite/ChairApp/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from . import urls
 from django.views.generic import TemplateView
 from .models import Room
 from .forms import updateOccupancyForm
+from twilio.twiml.messaging_response import MessagingResponse
 
 class ShowRooms(TemplateView):
     def get(self, request):
         rooms = Room.objects.all()
         return render(request, 'rooms.html',{'rooms':rooms})
 
-class UpdateRooms(TemplateView):
-    def get(self, request):
-        room = Room.objects.get(pk=1)
-        form = updateOccupancyForm()
-        return render(request, 'update.html', {'room':room, 'form':form})
-
+class sms(TemplateView):
     def post(self, request):
         room = Room.objects.get(pk=1)
-        form = updateOccupancyForm(request.POST)
-        if form.is_valid():
-            room.occupiedChairs = form.cleaned_data['occupiedChairs']
+        occupancy = request.POST.get('Body')
+        if int(occupancy) <= room.totalChairs:
+            room.occupiedChairs = int(occupancy)
             room.save()
-        return redirect('update')
+        else:
+            room.occupiedChairs = room.totalChairs
+            room.save()
+        return redirect('sms')
+
